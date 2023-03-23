9bf56eb11f1dd3993aa1f13ec7eaa3b7df2b8dff
Loitok <loitokrozrab@gmail.com>
2023-03-24 00:18:01 +0200

    initial commit

---
+++ BooksApp/Data/Configurations/BookConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using BooksApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BooksApp.Data.Configurations
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title)
+                .IsRequired();
+
+            builder.Property(x => x.PublicationDate)
+                .IsRequired();
+
+            builder.Property(x => x.PageCount)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+                .IsRequired();
+        }
+    }
+}
---
+++ BooksApp/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BooksApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksApp.Data.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
